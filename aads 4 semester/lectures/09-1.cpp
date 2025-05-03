2f2e9b574211f5ce7c2d5454e1a915e850507362
prestcote <vasilisaschvarz@gmail.com>
2025-05-03 21:40:16 +0300

    aads hw 9 (i think? maybe 8 tbh) done

--- aads 4 semester/lectures/09-1.cpp
+++ aads 4 semester/lectures/09-1.cpp
@@ -4,73 +4,134 @@
 
 class Graph {
 private:
-  std::vector<std::vector<int>> g;
-  size_t ln = 1;
-  std::vector<std::vector<int>> dp;
-  std::vector<int> h;
+  std::vector<std::vector<long long>> g;
+  size_t ln = 1LL;
+  std::vector<long long> list;
+  std::vector<long long> h;
+  std::vector<long long> used;
+  std::vector<long long>tree;
+  std::vector<long long> first;
 public:
   size_t size() {
     return g.size();
   }
   explicit Graph() = default;
-  Graph(size_t size, std::vector<std::vector<int>>& vec) {
+  Graph(std::vector<std::vector<long long>>& vec, long long root_) {
     g = vec;
-    //g.resize(size);
+    build(root_);
   }
-  std::vector<int>& operator[](size_t index) {
-    return g[index];
+  void dfs(long long v, long long height = 1) {
+    used[v] = 1;
+    h[v] = height;
+    list.push_back(v);
+    for (long long i : g[v])
+      if (!used[i])
+      {
+        dfs(i, height + 1);
+        list.push_back(v);
+      }
   }
-  void build(int root) {
-    while ((1 << ln) < size()) {
-      ++ln;
-    }
-    ln += 2;
-    h.resize(size());
-    dp.assign(size(), std::vector<int>(ln, root));
-    std::vector<int> used(size());
-    std::function<void(int, int)> dfs = [&](int v, int p) {
-      used[v] = 1;
-      dp[v][0] = p;
-      h[v] = h[p] + 1;
-      for (auto to : g[v]) {
-        if (!used[to]) {
-          dfs(to, v);
-        }
-      }
-      };
-    dfs(root, root);
-    for (int v = 0; v < size(); ++v) {
-      for (int k = 1; k < ln; ++k) {
-        dp[v][k] = dp[dp[v][k - 1]][k - 1];
-      }
+  
+  void build(long long i, long long l, long long r)
+  {
+    if (l == r)
+      tree[i] = list[l];
+    else
+    {
+      long long m = (l + r) >> 1;
+      build(i + i, l, m);
+      build(i + i + 1, m + 1, r);
+      if (h[tree[i + i]] < h[tree[i + i + 1]])
+        tree[i] = tree[i + i];
+      else
+        tree[i] = tree[i + i + 1];
     }
   }
 
-  int lca(int u, int v) {
-    if (h[u] > h[v]) {
-      std::swap(u, v);
+  void build(long long root)
+  {
+    long long n = (long long)g.size();
+    h.resize(n);
+    list.reserve(n * 2);
+    used.assign(n, 0);
+
+    dfs(root);
+    long long m = (long long)list.size();
+    tree.assign(list.size() * 4 + 1, -1);
+    build(1, 0, m - 1);
+
+    first.assign(n, -1);
+    for (long long i = 0; i < m; ++i)
+    {
+      long long v = list[i];
+      if (first[v] == -1)
+        first[v] = i;
     }
-    for (int k = ln - 1; k >= 0; --k) {
-      int p = dp[v][k];
-      if (h[p] >= h[u]) {
-        v = p;
-      }
-    }
-    if (u == v) {
-      return v;
-    }
-    for (int k = ln - 1; k >= 0; --k) {
-      if (dp[v][k] != dp[u][k]) {
-        v = dp[v][k];
-        u = dp[u][k];
-      }
-    }
-    return dp[v][0];
+  }
+
+  long long lca_tree_min(long long i, long long sl, long long sr, long long l, long long r)
+  {
+    if (sl == l && sr == r)
+      return tree[i];
+    long long sm = (sl + sr) >> 1;
+    if (r <= sm)
+      return lca_tree_min(i + i, sl, sm, l, r);
+    if (l > sm)
+      return lca_tree_min(i + i + 1, sm + 1, sr, l, r);
+    long long ans1 = lca_tree_min(i + i, sl, sm, l, sm);
+    long long ans2 = lca_tree_min(i + i + 1, sm + 1, sr, sm + 1, r);
+    return h[ans1] < h[ans2] ? ans1 : ans2;
+  }
+
+  long long lca(long long a, long long b)
+  {
+    long long left = first[a];
+    long long right = first[b];
+    if (left == -1 || right == -1) return -1;
+    if (left > right)  std::swap(left, right);
+    return lca_tree_min(1, 0, (long long)list.size() - 1, left, right);
   }
 };
 
 int main() {
-  std::vector<std::vector<int>> g;
-
+  long long n = 0;
+  long long m = 0;
+  std::cin >> n >> m;
+  if (n == 1) {
+    std::cout << 0;
+    return 0;
+  }
+  std::vector<std::vector<long long>> g(n);
+  long long q = 0;
+  //std::cout << "checkpoint 1\n";
+  for (long long i = 1LL; i < n; i++) {
+    std::cin >> q;
+    g[q].push_back(i);
+    g[i].push_back(q);
+  }
+  //std::cout << "checkpoint 2\n";
+  Graph graph(g, 0);
+  //std::cout << "checkpoint 3\n";
+  long long a1 = 0;
+  long long a2 = 0;
+  long long x = 0;
+  long long y = 0;
+  long long z = 0;
+  std::vector<long long> a(2 * m + 1);
+  std::cin >> a[1] >> a[2] >> x >> y >> z;
+  //std::cout << "checkpoint 4\n";
+  for (long long i = 3; i <= 2 * m; i++) {
+    a[i] = (x * a[i - 2] * 1LL + y * a[i - 1] * 1LL + z) % n;
+  }
+  //std::cout << "checkpoint 5\n";
+  long long v = graph.lca(a[1], a[2]);
+  //std::cout << "checkpoint 6\n";
+  long long ans = v;
+  for (long long i = 2; i <= m; i++) {
+    v = graph.lca((a[2 * i - 1] + v) % n, a[2 * i]);
+    ans += v;
+  }
+  //std::cout << "checkpoint 7\n";
+  std::cout << ans;
   return 0;
 }
--- graphs 4 semester/8hw/c.cpp
+++ graphs 4 semester/8hw/c.cpp
@@ -4,6 +4,7 @@
 #include <algorithm>
 #include <climits>
 #include <string>
+#include <unordered_map>
 
 struct e {
   int to = 0;
@@ -14,10 +15,9 @@ struct e {
 
 int s = 0;
 int t;
-std::vector<int> parent, level;
+std::vector<int> level, ptr;
 
 bool bfs(const std::vector<std::vector<e>>& g) {
-  std::fill(parent.begin(), parent.end(), -1);
   std::fill(level.begin(), level.end(), -1);
   level[s] = 0;
   std::queue<int> q;
@@ -28,7 +28,6 @@ bool bfs(const std::vector<std::vector<e>>& g) {
     for (const auto& u : g[v]) {
       if (level[u.to] == -1 && u.flow < u.cap) {
         level[u.to] = level[v] + 1;
-        parent[u.to] = v;
         q.push(u.to);
       }
     }
@@ -40,7 +39,8 @@ int dfs(int v, int flow, std::vector<std::vector<e>>& g) {
   if (v == t || flow == 0) {
     return flow;
   }
-  for (auto& u : g[v]) {
+  for (int& i = ptr[v]; i < g[v].size(); ++i) {
+    e& u = g[v][i];
     if (level[u.to] == level[v] + 1 && u.flow < u.cap) {
       int df = dfs(u.to, std::min(flow, u.cap - u.flow), g);
       if (df > 0) {
@@ -56,8 +56,10 @@ int dfs(int v, int flow, std::vector<std::vector<e>>& g) {
 int dinic(std::vector<std::vector<e>>& g) {
   int max_flow = 0;
   while (bfs(g)) {
-    while (int flow = dfs(s, INT_MAX, g))
+    std::fill(ptr.begin(), ptr.end(), 0);
+    while (int flow = dfs(s, INT_MAX, g)) {
       max_flow += flow;
+    }
   }
   return max_flow;
 }
@@ -67,88 +69,90 @@ int main() {
   std::cin.tie(nullptr);
   std::cout.tie(nullptr);
 
-  int n = 0;
+  int n;
   std::cin >> n;
   int k = n + 28;
   t = k - 1;
+
   std::vector<std::vector<e>> g(k);
   std::vector<std::string> cards(n);
-  std::vector<std::vector<std::pair<int, char>>> letters(n);
-
+  std::unordered_map<int, char> letters;
+  std::string cc;
+  char a;
+  char b;
   for (int i = 0; i < n; i++) {
-    std::string a;
-    std::cin >> a;
-    cards[i] = a;
-
+    std::cin >> cc;
+    cards[i] = cc;
+    a = cc[0];
+    b = cc[1];
     g[s].push_back({ i + 1, (int)g[i + 1].size(), 1, 0 });
     g[i + 1].push_back({ s, (int)g[s].size() - 1, 0, 0 });
 
-    int a1 = n + 1 + (a[0] - 'a');
-    g[i + 1].push_back({ a1, (int)g[a1].size(), 1, 0 });
-    g[a1].push_back({ i + 1, (int)g[i + 1].size() - 1, 0, 0 });
-    letters[i].emplace_back(a1, a[0]);
+    int ltoi1 = n + 1 + (a - 'a');
+    g[i + 1].push_back({ ltoi1, (int)g[ltoi1].size(), 1, 0 });
+    g[ltoi1].push_back({ i + 1, (int)g[i + 1].size() - 1, 0, 0 });
+    letters[(i + 1) * k + ltoi1] = a;
 
-    if (a[0] != a[1]) {
-      int a2 = n + 1 + (a[1] - 'a');
-      g[i + 1].push_back({ a2, (int)g[a2].size(), 1, 0 });
-      g[a2].push_back({ i + 1, (int)g[i + 1].size() - 1, 0, 0 });
-      letters[i].emplace_back(a2, a[1]);
+    if (a != b) {
+      int ltoi2 = n + 1 + (b - 'a');
+      g[i + 1].push_back({ ltoi2, (int)g[ltoi2].size(), 1, 0 });
+      g[ltoi2].push_back({ i + 1, (int)g[i + 1].size() - 1, 0, 0 });
+      letters[(i + 1) * k + ltoi2] = b;
     }
   }
-  //std::cout << "checkpoint\n";
+
   std::string str;
   std::cin >> str;
   std::vector<int> cntr(26, 0);
-  for (char c : str) cntr[c - 'a']++;
-  //std::cout << "checkpoint\n";
-  for (int i = 0; i < 26; i++) {
+  for (char c : str) {
+    cntr[c - 'a'] += 1;
+  }
+
+  for (int i = 0; i < 26; ++i) {
     if (cntr[i] > 0) {
       int ltoi = n + 1 + i;
       g[ltoi].push_back({ t, (int)g[t].size(), cntr[i], 0 });
       g[t].push_back({ ltoi, (int)g[ltoi].size() - 1, 0, 0 });
     }
   }
-  //std::cout << "checkpoint\n";
-  parent.resize(k);
+
   level.resize(k);
+  ptr.resize(k);
   int max_flow = dinic(g);
-  //std::cout << "checkpoint\n";
-  if (max_flow < (int)str.size()) {
+
+  if (max_flow != str.size()) {
     std::cout << "IMPOSSIBLE";
     return 0;
   }
-  //std::cout << "checkpoint\n";
-  std::vector<int> ans;
+  char c;
   std::vector<std::vector<int>> f(26);
-  //std::cout << "checkpoint\n";
-  for (int i = 0; i < n; i++) {
-    for (auto& p : letters[i]) {
-      int ltoi = p.first;
-      char c = p.second;
-      for (auto& u : g[i + 1]) {
-        if (u.to == ltoi && u.flow > 0) {
-          f[c - 'a'].push_back(i);
-          break;
-        }
+  for (int i = 1; i <= n; ++i) {
+    for (const auto& edge : g[i]) {
+      if (edge.flow > 0 && edge.to > n) {
+        c = letters[i * k + edge.to];
+        f[c - 'a'].push_back(i);
       }
     }
   }
-  //std::cout << "checkpoint\n";
-  std::vector<int> ptr(26, 0);
+
+  std::vector<int> ans;
+  std::vector<int> card_ptr(26, 0);
   for (char c : str) {
-    if (ptr[c - 'a'] >= (int)f[c - 'a'].size()) {
+    int ci = c - 'a';
+    if (card_ptr[ci] >= f[ci].size()) {
       std::cout << "IMPOSSIBLE";
       return 0;
     }
-    int ind = f[c - 'a'][ptr[c - 'a']++];
-    if (cards[ind][0] == c) {
-      ans.push_back(ind + 1);
+    int ind = f[ci][card_ptr[ci]++];
+    auto& card = cards[ind - 1];
+    if (card[0] == c) {
+      ans.push_back(ind);
     }
     else {
-      ans.push_back(-(ind + 1));
+      ans.push_back(-ind);
     }
   }
-  //std::cout << "checkpoint\n";
+
   for (int i = 0; i < ans.size(); i++) {
     std::cout << ans[i] << ' ';
   }
